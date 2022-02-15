ecbc467638b0e2fc41948dbb77807803836c6daf
salsitu <sals@itu.dk>
2022-02-15 01:47:45 +0100

    Red exercises

--- Program.fs
+++ Program.fs
@@ -17,6 +17,7 @@ let rec downto1 n =
         []
     else
         n :: downto1 (n - 1)
+
 let rec downto2 =
     function
     | n when n <= 0 -> []
@@ -39,8 +40,10 @@ let rec combinePair =
 // Exercise 2.4
 exception Undefined of string
 type complex = float * float
+
 let mkComplex : float -> float -> complex = fun a b -> (a, b)
 let complexToPair : complex -> float * float = fun c -> c
+
 let (|+|) : complex -> complex -> complex = fun (a, b) (c, d) -> (a + c, b + d)
 let (|*|) : complex -> complex -> complex = fun (a, b) (c, d) -> (a * c - b * d, b * c + a * d)
 let (|-|) : complex -> complex -> complex = fun (a, b) (c, d) -> (a, b) |+| (-c, -d)
@@ -63,8 +66,8 @@ let implodeRev (cs : char list) = List.fold (fun s c -> string c + s) "" cs
 
 // Exercise 2.7
 let toUpper = explode1 >> List.map System.Char.ToUpper >> implode
-let toUpper2 = implode << List.map System.Char.ToUpper << explode1
-let toUpper3 s = explode1 s |> List.map System.Char.ToUpper |> implode
+let toUpper2 = implode << List.map System.Char.ToUpper << explode1 // alternative implementation
+let toUpper3 s = explode1 s |> List.map System.Char.ToUpper |> implode // another alternative implementation
 
 // Exercise 2.8
 exception AckNegative of string
@@ -86,6 +89,7 @@ let time f =
     let res = f ()
     let finish = System.DateTime.Now
     (res, finish - start)
+
 let timeArg1 f a = time (fun () -> f a)
 
 // Exercise 2.10
@@ -93,6 +97,7 @@ let downto3 f n e =
     match n with
     | n when n > 0 -> List.foldBack f [1..(n-1)] (f n e)
     | _ -> e
+
 let fac n = downto3 (*) n 1
 let range g n = downto3 (fun m e -> g m :: e) n []
 
@@ -102,10 +107,11 @@ let hello : word = [('H', 4); ('E', 1); ('L', 1); ('L', 1); ('O', 1)]
 
 // Exercise 2.12
 type squareFun = word -> int -> int -> int
-let pointHelper (c, p) = p
-let singleLetterScore : squareFun = fun w pos acc -> pointHelper w.[pos] + acc
-let doubleLetterScore : squareFun = fun w pos acc -> pointHelper w.[pos] * 2 + acc
-let tripleLetterScore : squareFun = fun w pos acc -> pointHelper w.[pos] * 3 + acc
+let extractPoints (c, p) = p
+
+let singleLetterScore : squareFun = fun w pos acc -> extractPoints w.[pos] + acc
+let doubleLetterScore : squareFun = fun w pos acc -> extractPoints w.[pos] * 2 + acc
+let tripleLetterScore : squareFun = fun w pos acc -> extractPoints w.[pos] * 3 + acc
 
 // Exercise 2.13
 let doubleWordScore : squareFun = fun _ _ acc -> acc * 2
@@ -116,3 +122,38 @@ let oddConsonants : squareFun = fun w pos acc ->
     let consonants = ['B'; 'C'; 'D'; 'F'; 'G'; 'H'; 'J'; 'K'; 'L'; 'M'; 'N'; 'P'; 'Q'; 'R'; 'S'; 'T'; 'V'; 'W'; 'X'; 'Z']
     let checkOdd = List.fold (fun s l -> if List.contains ((fun (a, b) -> a) l) consonants then not s else s) false
     if checkOdd w then -acc else acc
+
+
+(*
+    RED
+*)
+
+// Exercise 2.15
+type square = (int * squareFun) list
+
+let SLS : square = [(0, singleLetterScore)]
+let DLS : square = [(0, doubleLetterScore)]
+let TLS : square = [(0, tripleLetterScore)]
+
+let DWS : square = SLS @ [(1, doubleWordScore)]
+let TWS : square = SLS @ [(1, tripleWordScore)]
+
+let calculatePoints : square list -> word -> int = fun squares w ->
+    let extractFirst (x, _) = x
+    let extractSecond (_, x) = x
+    let applyWord i (priority, f) = (priority, f w i)
+    let calculate =
+        ((List.mapi (fun i sq -> List.map (applyWord i) sq))
+        >> (List.fold (fun s sq -> sq @ s) [])
+        >> (List.sortBy extractFirst)
+        >> (List.map extractSecond)
+        >> (List.fold (fun s f -> f s)) 0)
+    calculate squares
+
+let calculatePoints2 : square list -> word -> int = fun squares w -> // alternative implementation
+    squares
+    |> List.mapi (fun i sq -> List.map (fun (priority, f) -> (priority, f w i)) sq)
+    |> List.fold (fun s sq -> sq @ s) []
+    |> List.sortBy (fun (a, b) -> a)
+    |> List.map (fun (a, b) -> b)
+    |> List.fold (fun s f -> f s) 0
